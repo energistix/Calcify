827138157cad2f4363d24530f6b947b321867415
energistix <50068022+energistix@users.noreply.github.com>
2025-02-15 11:18:31 +0100

    evaluation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
+import { evaluate as mathEvaluate } from "mathjs";
+
 const input = document.getElementById("input") as HTMLTextAreaElement;
 const output = document.getElementById("output") as HTMLDivElement;
 
 input.addEventListener("input", () => {
   let res = "";
+  const scope = {};
   input.value.split("\n").forEach((line) => {
-    console.log(line);
-    res += " ".repeat(line.length + 1) + line + "\n";
+    if (line === "") return (res += "\n");
+    let output = "";
+    try {
+      let evaluation = mathEvaluate(line, scope).toString();
+      if (evaluation.trim().includes("\n")) output = "error";
+      else output = evaluation;
+    } catch (e) {
+      output = "error";
+    }
+    res += " ".repeat(line.length + 1) + output + "\n";
   });
   output.innerHTML = res;
 });
